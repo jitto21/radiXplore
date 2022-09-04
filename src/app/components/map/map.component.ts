b370780a7fc94ca6fb7a4c4f6669488621ff96e5
jitto21 <jittotp@gmail.com>
2022-09-04 12:04:38 +0530

    Add scroll to map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapServiceService } from 'src/app/services/map-service.service';
-import  * as L from 'leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -9,29 +9,51 @@ import  * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
 
-  map: any
+  map: any;
+  locationData: any;
 
   constructor(private mapService: MapServiceService) { }
 
   ngOnInit(): void {
     this.mapService.getMapData().subscribe((data: any) => {
-      if(this.map) {
+      this.locationData = data;
+      if (this.map) {
         this.map.remove();
       }
       console.log(data);
-      const lat = +data.latitude;
-      const long = +data.longitude;
-      console.log("lat: ", lat, " ,long: ", long);
-      this.map = L.map('map').setView([lat, long], 13);
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-      }).addTo(this.map);
-      if(lat && long) {
-        var marker = L.marker([lat , long]).addTo(this.map);
-      }
-
+      setTimeout(()=> {
+        if (this.locationData && this.locationData.latitude && this.locationData.longitude) {
+          const lat = +this.locationData.latitude;
+          const long = +this.locationData.longitude;
+          console.log("lat: ", lat, " ,long: ", long);
+          this.map = L.map('map').setView([lat, long], 13);
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+          }).addTo(this.map);
+          const icon = L.icon({iconUrl: '../../../assets/images/marker.png', iconSize: [20,25]})
+          var marker = L.marker([lat, long], {icon}).addTo(this.map);
+          document.getElementById("map")?.scrollIntoView();
+        }
+      })
     })
+  }
+
+  getCoordinates(lat: string, long: string): string {
+    let coordinates = '';
+    if (+long >= 0 && +long <= 180) {
+      coordinates += long + "\xB0 North and ";
+    }
+    else if (+long <= 0 && +long >= -180) {
+      coordinates += long + "\xB0 South and ";
+    }
+    if (+lat >= 0 && +lat <= 90) {
+      coordinates += lat + "\xB0 East";
+    }
+    else if (+lat <= 0 && +lat >= -90) {
+      coordinates += lat + "\xB0 West";
+    }
+    return coordinates;
   }
 
 }
